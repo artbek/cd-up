7f3dbf9e3e2e2e15854cea4a6ff2ec6272ccab5f
bart <bart@bart-Inspiron-530.(none)>
2012-05-30 08:56:33 +0100

    Correct whitespaces.

--- cdup.sh
+++ cdup.sh
@@ -20,17 +20,17 @@ cdup() {
 }
 
 _cdup_complete() {
-    local cur prev path ar
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    path=$(pwd)
-    old_ifs=$IFS
-    IFS=/
-    ar=$path
-    COMPREPLY=( $(compgen -W "${ar}" ${cur}) )
-    IFS=$old_ifs
-    return 0
+	local cur prev path ar
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	path=$(pwd)
+	old_ifs=$IFS
+	IFS=/
+	ar=$path
+	COMPREPLY=( $(compgen -W "${ar}" ${cur}) )
+	IFS=$old_ifs
+	return 0
 }
 
 complete -F _cdup_complete cdup
